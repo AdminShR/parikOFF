ddded152de9b02d85c34686a4850c1b3e914d0e5
Usmonov Shavkatjon <57574675+AdminShR@users.noreply.github.com>
2021-05-07 12:35:27 +0500

    dfgefgfdg

---
+++ index.php
@@ -0,0 +1,223 @@
+<!DOCTYPE html>
+<html lang="ru" class="html">
+<head>
+	<meta charset="utf-8">
+	<title>ParikOFF активатор роста волос</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+	<meta name="viewport" content="width=480">
+
+   
+	    <!-- Favicon -->
+    <link rel="icon" href="img/products_new/4192.png" type="image/png">
+    <link rel="shortcut icon" href="img/products_new/4192.png" type="image/png">
+
+    <!-- OG -->
+    <meta property="og:url" content="https://ma-parikoff.lovely-health.com/" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="ParikOFF активатор роста волос" />
+    <meta property="og:image" content="https://ma-parikoff.lovely-health.com/img/products_new/4192.png" />
+    <meta property="og:image:width" content="150" />
+    <meta property="og:image:height" content="150" />
+    <meta property="og:site_name" content="ParikOFF активатор роста волос" />
+    <meta property="og:description" content="ParikOFF активатор роста волос" />
+
+    
+	<!-- CSS -->
+	<link rel="stylesheet" type="text/css" href="files/parikoff_mobile_2/css/style.css">
+
+	<!-- JS -->
+	<script type="text/javascript" src="files/_js/jquery-1.10.2.min.js" ></script>
+	<script type="text/javascript" src="files/_js/ab.min.js"></script>
+	<script type="text/javascript" src="files/_js/validation.js"></script>
+	
+	<script type="text/javascript" src="files/parikoff_mobile_2/js/main.js"></script>
+    
+
+	<script type="text/javascript">$jsonData = {"product":{"id":4192,"name":"ParikOFF активатор роста волос","title":"ParikOFF активатор роста волос","short_description":"ParikOFF активатор роста волос","full_description":"ParikOFF активатор роста волос"},"prices":{"270":{"price":249,"old_price":498,"delivery_price":0,"tax_price":0,"upsale_price":0,"geo_key":"UA","name":"Україна","currency":"грн.","rate":0.37,"phone_template":"+38 067 927 25 74","address_template":"01024, г. Кiев, ул. Богомольца, д.5, кв.9","name_template":"Карпенко Ярослав Федорович","active":true},"356":{"price":9500,"old_price":19000,"delivery_price":0,"tax_price":0,"upsale_price":0,"geo_key":"AM","name":"Հայաստան","currency":"драм","rate":12.1,"phone_template":"+37 499 35 22 62","address_template":"0010, Ереван, ул. Вардананц, д. 3, кв. 54","name_template":"Григорян Ашот Багдасарович","active":false},"481":{"price":99000,"old_price":198000,"delivery_price":0,"tax_price":0,"upsale_price":0,"geo_key":"UZ","name":"Ўзбекистон","currency":"UZS","rate":46.62,"phone_template":"+998 123123123","address_template":"","name_template":"Anvar","active":false}},"lowPrice":{"year":"2021","month":"05","day":"08"}};</script>
+</head>
+<body class="t-body">
+<script type="text/javascript">
+  var landing_notifiers = new Object(),
+    price, currency, delivery_price;
+
+  for (key in $jsonData.prices) {
+    if ($jsonData.prices[key].active) {
+      price = $jsonData.prices[key].price;
+      currency = $jsonData.prices[key].currency;
+      delivery_price = $jsonData.prices[key].price + $jsonData.prices[key].delivery_price;
+    }
+  }
+
+  landing_notifiers.params = {
+    city: "",
+    landDir: "parikoff_mobile_2",
+    sex: "",
+    multiply: "" || 0,
+    no_price: "" || false,
+    delay: ("" * 1000) || 12000,
+    price: price || 0,
+    currency: currency || "руб.",
+    delivery_price: delivery_price,
+    geoKey: "" || "RU",
+    zdorov: "" || false
+  };
+</script>
+
+<script type="text/javascript" src="files/_blocks/notificators/func.js@11"></script>
+
+<script>
+  var formIsSubmitted = false;
+</script>
+
+<script type="text/javascript" src="files/_blocks/notificators/unload_submit.js@7"></script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                
+                                                                                                                                                                                               
+                     
+    
+
+    <div class="button"><a href="#order">Buyurtma qoldirish</a></div>
+    <div class="logo"><img src="files/parikoff_mobile_2/img/logo.png" /></div>
+    <div class="brosayte"><b>CHIROYLI SOCH YOKI KO'RKAM SOQOL TEZ O'STIRMOQCHIMISIZ?</b></div>
+    <div class="nikostop"><img src="files/parikoff_mobile_2/img/nico.jpg" /></div>
+
+    <div class="skidka"><span>bugun chegirma <b>50%</b></span>
+        <h4>
+            <span class="price_land_s2">498</span> <span class="price_land_curr">грн.</span>
+        </h4>
+        <h3>
+            <span class="price_land_s1">249</span>&nbsp;<span class="price_land_curr">грн.</span>
+        </h3>
+    </div>
+    <div class="text_">Sprey <b>ParikOFF</b> - tez va effektiv tarzda soch to'kilishi bilan kurashadi. Tabiiy kompleks
+        shikastlangan va zaiflashib qolgan sochlarni tiklaydi. O'rganib qolishga olib kelmaydi va yon ta'siri yo'q.
+        Natijaga erisish uchun to'liq kurs o'tish zarur. Kurs - 30 kun.</div>
+    <div class="skidka">
+        <h3>FIKRLAR:</h3>
+    </div>
+    <div class="nikostop"><img src="files/parikoff_mobile_2/img/gotov.jpg" /></div>
+    <div class="text_"><b>ParikOFF</b> - soch, qosh va soqol o'stirish uchun eng yaxshi aktivatori.</div>
+    <div class="forma">
+        <div class="message"></div>
+        <form class="order-form orderformcdn" id="order" action="order.php" method="post">
+<input type="hidden" name="sub1" value="<?php echo htmlspecialchars($_GET['utm_source']); ?>">
+<input type="hidden" name="sub2" value="<?php echo htmlspecialchars($_GET['utm_medium']); ?>">
+<input type="hidden" name="sub3" value="<?php echo htmlspecialchars($_GET['utm_campaign']); ?>">
+<input type="hidden" name="sub4" value="<?php echo htmlspecialchars($_GET['utm_content']); ?>">
+<input type="hidden" name="sub5" value="<?php echo htmlspecialchars($_GET['utm_term']); ?>">
+<input type="hidden" name="fbpx" value="<?php echo htmlspecialchars($_GET['fbpx']); ?>">
+
+                        <select name="country" id='country' class="country_select form-control country_chang">
+                                    <option value='270' selected="selected">Україна</option>
+                                    <option value='356'>Հայաստան</option>
+                                    <option value='481'>Ўзбекистон</option>
+                            </select>
+            
+            <label>Ism (m-u: Shavkat Rahmanov)</label>
+            <input class="form-control" placeholder="" id="name" name="name" type="text" maxlength="255" />
+            <label>Telefon</label>
+            <div class="input_tel">
+                <input placeholder="" class="form-control" id="phone" name="phone" type="text" maxlength="25" />
+            </div>
+            <input type="hidden" name="address" value="">
+            <input type="hidden" name="product_count" id="product_count"  value="1">
+            <input type="hidden" name="number_product"  value="4192">
+            <input type="hidden" name="split_test_id" 	value="0">
+<input type="hidden" name="split_test" 		value="0">
+<input type="hidden" name="split_test_host" value="ma-parikoff.lovely-health.com">
+<input type="hidden" name="fchck"           value="4f94ba44abf97ec52892386831ad702e63809de3">
+<input type="hidden" name="ucfi" id="ucfi"  value="0">
+            <input class="submitBtn" type="submit" value="Buyurtma qoldirish" />
+        </form>
+    </div>
+     <div class="copy">
+        <div class="copyright"><img src="files/_blocks/copyright/img/rekv.png" style="display:block;margin:0 auto">
+
+<div style="text-align: right"><img src="files/_blocks/copyright/img/1001transits.png" style="display: inline-block !important;" /></div><br><a href="privacypolicy.html" target="_blank">Политика конфиденциальности</a></div>
+    </div>
+
+	
+<!--{-->
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript">
+(function (d, w, c) {
+    (w[c] = w[c] || []).push(function() {
+        try {
+            w.yaCounter22765945 = new Ya.Metrika({id:22765945,
+                    webvisor:true,
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true});
+        } catch(e) { }
+    });
+ 
+    var n = d.getElementsByTagName("script")[0],
+        s = d.createElement("script"),
+        f = function () { n.parentNode.insertBefore(s, n); };
+    s.type = "text/javascript";
+    s.async = true;
+    s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
+ 
+    if (w.opera == "[object Opera]") {
+        d.addEventListener("DOMContentLoaded", f, false);
+    } else { f(); }
+})(document, window, "yandex_metrika_callbacks");
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/22765945" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+<!--||-->
+<!--<script async src="https://fkthe.com/p.js"></script>-->
+<script>
+(function (d, w) {
+    var params = '';
+    if (typeof w.orderIdFull !== 'undefined') {
+        params = '?id=' + w.orderIdFull;
+    }
+    
+    var s = d.createElement("script"),
+        f = function () { d.body.appendChild(s); };
+    s.type = "text/javascript";
+    s.async = true;
+    s.src ="https://fkthe.com/p.js" + params;
+
+    if (w.opera == "[object Opera]") {
+        d.addEventListener("DOMContentLoaded", f, false);
+    } else { f(); }
+})(document, window);
+
+</script>
+<!--}-->
+
+	<?php if (!empty($_GET['fbpx'])): ?><!-- Facebook Pixel Code --><script>!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version="2.0";n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window, document,"script","https://connect.facebook.net/en_US/fbevents.js");fbq("init", "<?php echo htmlspecialchars($_GET['fbpx']); ?>");fbq("track", "PageView");</script><noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=<?php echo htmlspecialchars($_GET['fbpx']); ?>&ev=PageView&noscript=1"/></noscript><!-- End Facebook Pixel Code --><?php endif; ?>
+</body>
+</html>
+
